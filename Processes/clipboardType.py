6a2f1106e4866149df46c94bfe02d8ee6e7577c2
ShantanuVichare <sheradgrr54@gmail.com>
2025-07-19 15:27:54 -0600

    adjusted clipboard type delay

--- Processes/clipboardType.py
+++ Processes/clipboardType.py
@@ -11,6 +11,8 @@ def run_clip_to_type():
     logger.log(f"Typing clipboard text. Length: {len(text)} chars")
     kb = keyboard.Controller()
     for char in text:
+        default_delay = 0.01  # Default delay between key presses
+        extra_delay = 0.0
         if char == '\r' or char == '\x00' or char == '\x0b' or char == '\x0c':
             # Ignored characters: 
             #   \r (carriage return),
@@ -21,14 +23,17 @@ def run_clip_to_type():
         elif char == '\n':
             kb.press(keyboard.Key.enter)
             kb.release(keyboard.Key.enter)
+            extra_delay = 0.05
         elif char == '\t':
             kb.press(keyboard.Key.tab)
             kb.release(keyboard.Key.tab)
+            extra_delay = 0.05
         elif char == ' ':
             kb.press(keyboard.Key.space)
             kb.release(keyboard.Key.space)
+            extra_delay = 0.05
         else:
             # For other characters, just type them directly
             kb.press(char)
             kb.release(char)
-        time.sleep(0.1)
+        time.sleep(default_delay + extra_delay)
